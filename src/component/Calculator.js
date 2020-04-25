0ca6dccf23836d9c9e58a8d88d2a05cf238d719d
defaulterrr <sp@zero.ru>
2020-04-25 18:05:04 +0300

    another faulty commit

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -4,38 +4,7 @@ import ActionButton from './ActionButton/ActionButton'
 import calculatorModel from './../model/CalculatorModel'
 
 class Calculator extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            firstValue:     0,
-            secondValue:    0,
-            result:         0,
-        }
-        this.onPress = this.onPress.bind(this)
-        
-    }
-
-    onPress = (label) => {
-        const num1 = Number(document.getElementById("num_1").value)
-        const num2 = Number(document.getElementById("num_2").value)
-        this.setState({
-            result:     calculatorModel(label,num1,num2)
-        }) 
-    }  
     
-    render() {
-        return (
-            <div className="Calculator">
-                <ValueBox label=" First Value: " id="num_1" value={this.state.firstValue}></ValueBox>
-                <ValueBox label="Second Value: " id="num_2" value={this.state.secondValue}></ValueBox>
-                <ActionButton label="+" action={this.onPress}></ActionButton>
-                <ActionButton label="-" action={this.onPress}></ActionButton>
-                <ActionButton label="/" action={this.onPress}></ActionButton>
-                <ActionButton label="*" action={this.onPress}></ActionButton>
-        <h1>Answer: {this.state.result}</h1>
-            </div>
-        )
-    }
 }
 
 export default Calculator;
